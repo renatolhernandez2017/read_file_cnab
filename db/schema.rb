2c29588c9efd745982668543fb6407ac775269d4
Renato Hernandez <renatolhernandez@hotmail.com>
2025-02-04 13:01:26 +0000

    Ajustar layout principal e rodapé

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_30_151613) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_04_125200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "kind"
+    t.date "date_ocurrence"
+    t.decimal "price"
+    t.string "cpf"
+    t.string "card"
+    t.string "store_owner"
+    t.string "store_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.time "time_ocurrence"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,8 +41,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_30_151613) do
     t.string "name"
     t.string "role"
     t.string "image"
+    t.string "hash_link"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "transactions", "users"
 end
---
+++ test/controllers/uploads_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class UploadsControllerTest < ActionDispatch::IntegrationTest
+end
